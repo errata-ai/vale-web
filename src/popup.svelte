7a08c61cb1e68b3f953adbfe8f2d5b80b9abda94
jdkato <joseph@jdkato.io>
2024-01-28 05:41:33 -0800

    WIP

--- src/popup.svelte
+++ src/popup.svelte
@@ -4,25 +4,55 @@
 
   import "./style.css"
 
-  var status = "idle"
-
   onMount(() => {
     browser.runtime.sendMessage({
       action: "get_source"
     })
+  })
 
-    browser.runtime.onMessage.addListener(function (request) {
-      if (request.action === "analysis_complete") {
-        console.log(request)
-        status = "complete"
+  browser.runtime.onMessage.addListener(function (req) {
+    if (req.action === "analysis_complete") {
+      /*for (alert in req.alerts) {
+        var a = req.alerts[alert]
       }
-    })
+      console.log(rows)*/
+    }
   })
 </script>
 
-<div>
-  <h2 class="text-center text-red-100">Welcome to your Vale Extension!</h2>
-  <p class="text-center text-red-100">
-    {status}
-  </p>
+<div id="wrapper" class="popup">
+  <table class="table-fixed">
+    <thead>
+      <tr>
+        <th>Song</th>
+        <th>Artist</th>
+        <th>Year</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+        <td>Malcolm Lockyer</td>
+        <td>1961</td>
+      </tr>
+      <tr>
+        <td>Witchy Woman</td>
+        <td>The Eagles</td>
+        <td>1972</td>
+      </tr>
+      <tr>
+        <td>Shining Star</td>
+        <td>Earth, Wind, and Fire</td>
+        <td>1975</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
+
+<style>
+  .popup {
+    min-width: 400px;
+    overflow: auto;
+    max-height: 400px;
+  }
+</style>
